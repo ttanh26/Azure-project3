1de1969045289ae08bb4b97b8abf6f055fdf4121
ttanh26 <ttanh26@gmail.com>
2022-11-25 16:22:32 +0700

    reverse ubuntu version

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,7 +29,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
