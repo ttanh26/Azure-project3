3b8de88ad0a7d46c8dc9d155a7cee3b00b0ff3c8
ttanh26 <ttanh26@gmail.com>
2022-11-26 17:28:43 +0700

    update selenium log

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from datetime import datetime
 
+def _get_timestamp():
+    return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    
 
 # Start the browser and login with standard_user
 def login (user, password):
@@ -11,37 +15,37 @@ def login (user, password):
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
-    driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver = webdriver.Chrome(options=options, executable_path='/path/to/chromedriver')
+    print (_get_timestamp(), 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
     driver.find_element(By.ID, 'user-name').send_keys(user)
     driver.find_element(By.ID, 'password').send_keys(password)
     driver.find_element(By.ID, 'login-button').click()
 
-    print(f'Login with username {user} and password {password}')
+    print(_get_timestamp(), f'Login with username {user} and password {password}')
     product_list = driver.find_elements(By.CSS_SELECTOR, "div[class=inventory_item] > div.inventory_item_description > div.pricebar > button")
     c = 0 
     for item in product_list:
         print('Adding new item to cart!')
-        item_name = item.find_element(By.CLASS_NAME, )
         item.click()
         c += 1
-    print(f"There are {c} items added to cart!")
 
-    print('Removing items from cart!')
+    print(_get_timestamp(), f"There are {c} items added to cart!")
+
+    print(_get_timestamp(), 'Removing items from cart!')
     
     product_list = driver.find_elements(By.CSS_SELECTOR, "div[class=inventory_item] > div.inventory_item_description > div.pricebar > button[class='btn btn_secondary btn_small btn_inventory']")
     c = 0 
     for item in product_list:
-        print('Removing an item from cart!')
+        print(_get_timestamp(), 'Removing an item from cart!')
         item.click()
         c += 1
 
-    print(f'There are {c} items removed from cart!')
-    
+    print(_get_timestamp(), f'There are {c} items removed from cart!')
+
     driver.close()
+    print(_get_timestamp(), 'Closed browser sucessfully!')
 
 login('standard_user', 'secret_sauce')
 
